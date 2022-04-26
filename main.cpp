b230411630ebbd92172f4235acac64aaef2be6ef
Sel-Kata <60038461+Sel-Kata@users.noreply.github.com>
2022-04-27 00:26:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+> Оопэшный классифицированный плюсовичок:
 #include <iostream>
 #include <vector>
+#include <string>
+#include <queue>
+#include <set>
 
 // 1 - white, 2 - yellow, 3 - pink, 4 - green, 5 - orange, 6 - blue
 
-class cube{
+class Cube{
 private:
     int front_[3][3];
     int back_[3][3];
@@ -51,7 +55,69 @@ public:
         down_[i][j] = value;
     }
 
-    cube(int front[3][3], int back[3][3], int right[3][3], int left[3][3], int up[3][3], int down[3][3]){
+    bool operator == (const Cube& other)
+    {
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                if (this->front_[i][j] != other.front_[i][j])
+                    return false;
+                if (this->back_[i][j] != other.back_[i][j])
+                    return false;
+                if (this->right_[i][j] != other.right_[i][j])
+                    return false;
+                if (this->left_[i][j] != other.left_[i][j])
+                    return false;
+                if (this->up_[i][j] != other.up_[i][j])
+                    return false;
+                if (this->down_[i][j] != other.down_[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator != (const Cube& other)
+    {
+        return !(this->operator==(other));
+    }
+
+    Cube& operator = (const Cube& other)
+            {
+                for (int i = 0; i < 3; ++i){
+                    for (int j = 0; j < 3; ++j){
+                        set_front(i, j, other.front_[i][j]);
+                        set_back(i, j, other.back_[i][j]);
+                        set_right(i, j, other.right_[i][j]);
+                        set_left(i, j, other.left_[i][j]);
+                        set_up(i, j, other.up_[i][j]);
+                        set_down(i, j, other.down_[i][j]);
+                    }
+                }
+                return *this;
+            }
+
+    Cube()
+    {
+        int front[3][3] = {{6, 6, 6}, {6, 6, 6}, {6, 6, 6}};
+        int back[3][3] = {{4, 4, 4}, {4, 4, 4}, {4, 4, 4}};
+        int right[3][3] = {{3, 3, 3}, {3, 3, 3}, {3, 3, 3}};
+        int left[3][3] = {{5, 5, 5}, {5, 5, 5}, {5, 5, 5}};
+        int up[3][3] = {{2, 2, 2}, {2, 2, 2}, {2, 2, 2}};
+        int down[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                set_front(i, j, front[i][j]);
+                set_back(i, j, back[i][j]);
+                set_right(i, j, right[i][j]);
+                set_left(i, j, left[i][j]);
+                set_up(i, j, up[i][j]);
+                set_down(i, j, down[i][j]);
+            }
+        }
+    }
+
+    Cube(int front[3][3], int back[3][3], int right[3][3], int left[3][3], int up[3][3], int down[3][3]){
         for (int i = 0; i < 3; ++i){
             for (int j = 0; j < 3; ++j){
                 set_front(i, j, front[i][j]);
@@ -67,7 +133,8 @@ public:
     void right_rotation_up(){
         int up1[3], up2[3], back1[3], back2[3], down1[3], down2[3], front1[3], front2[3];
 
-        for (int i = 0; i < 3; ++i){
+> Оопэшный классифицированный плюсовичок:
+for (int i = 0; i < 3; ++i){
             up1[i] = get_up(i, 2);
             back1[i] = get_back(i, 2);
             down1[i] = get_down(i, 2);
@@ -91,30 +158,9 @@ public:
         }
     }
     void right_rotation_down(){
-        int up1[3], up2[3], back1[3], back2[3], down1[3], down2[3], front1[3], front2[3];
-
-        for (int i = 0; i < 3; ++i){
-            up1[i] = get_up(i, 2);
-            back1[i] = get_back(i, 2);
-            down1[i] = get_down(i, 2);
-            front1[i] = get_front(i, 2);
-
-            up2[i] = get_right(0, i);
-            back2[i] = get_right(i, 2);
-            down2[i] = get_right(2, i);
-            front2[i] = get_right(i, 0);
-        }
-        for (int i = 0; i < 3; ++i){
-            set_up(i, 2, back1[i]);
-            set_back(i, 2, down1[i]);
-            set_down(i, 2, front1[i]);
-            set_front(i , 2, up1[i]);
-
-            set_right(0, i, back2[i]);
-            set_right(i, 0, up2[i]);
-            set_right(2, i, front2[i]);
-            set_right(i, 2, down2[i]);
-        }
+        this->right_rotation_up();
+        this->right_rotation_up();
+        this->right_rotation_up();
     }
 
     void left_rotation_up(){
@@ -144,30 +190,9 @@ public:
         }
     }
     void left_rotation_down(){
-        int up1[3], up2[3], back1[3], back2[3], down1[3], down2[3], front1[3], front2[3];
-
-        for (int i = 0; i < 3; ++i){
-            up1[i] = get_up(i, 0);
-            back1[i] = get_back(i, 0);
-            down1[i] = get_down(i, 0);
-            front1[i] = get_front(i, 0);
-
-            up2[i] = get_left(0, i);
-            back2[i] = get_left(i, 0);
-            down2[i] = get_left(2, i);
-            front2[i] = get_left(i, 2);
-        }
-        for (int i = 0; i < 3; ++i){
-            set_up(i, 0, back1[i]);
-            set_back(i, 0, down1[i]);
-            set_down(i, 0, front1[i]);
-            set_front(i , 0, up1[i]);
-
-            set_left(0, i, back2[i]);
-            set_left(i, 0, down2[i]);
-            set_left(2, i, front2[i]);
-            set_left(i, 2, up2[i]);
-        }
+        this->left_rotation_up();
+        this->left_rotation_up();
+        this->left_rotation_up();
     }
 
     void up_rotation_right(){
@@ -183,42 +208,34 @@ public:
             back2[i] = get_up(0, i);
             left2[i] = get_up(i, 0);
         }
+
+        int front3[3], right3[3], back3[3], left3[3];
+        int counter = 2;
+
+        for (int i = 0; i < 3; i++)
+        {
+            front3[i] = front2[counter];
+            right3[i] = right2[counter];
+            back3[i] = back2[counter];
+            left3[i] = left2[counter];
+            counter--;
+        }
         for (int i = 0; i < 3; ++i){
             set_right(0, i, front1[i]);
             set_back(0, i, right1[i]);
             set_left(0, i, back1[i]);
             set_front(0, i, left1[i]);
 
-            set_up(i, 2, front2[i]);
-            set_up(0, i,right2[i]);
-            set_up(i, 0, back2[i]);
+            set_up(i, 0, back3[i]);////
             set_up(2, i, left2[i]);
+            set_up(i, 2, front3[i]);
+            set_up(0, i, right2[i]);
         }
-    }
+    }///
     void up_rotation_left(){
-        int front1[3], front2[3], right1[3], right2[3], back1[3], back2[3], left1[3], left2[3];
-        for (int i = 0; i < 3; ++i){
-            front1[i] = get_front(0, i);
-            right1[i] = get_right(0, i);
-            back1[i] = get_back(0, i);
-            left1[i] = get_left(0, i);
-
-            front2[i] = get_up(2, i);
-            right2[i] = get_up(i, 2);
-            back2[i] = get_up(0, i);
-            left2[i] = get_up(i, 0);
-        }
-        for (int i = 0; i < 3; ++i){
-            set_right(0, i, back1[i]);
-            set_back(0, i, left1[i]);
-            set_left(0, i, front1[i]);
-            set_front(0, i, right1[i]);
-
-            set_up(i, 2, back2[i]);
-            set_up(0, i,left2[i]);
-            set_up(i, 0, front2[i]);
-            set_up(2, i, right2[i]);
-        }
+        this->up_rotation_right();
+        this->up_rotation_right();
+        this->up_rotation_right();
     }
 
     void down_rotation_right(){
@@ -242,38 +259,17 @@ public:
             set_left(2, i, back1[i]);
             set_front(2, i, left1[i]);
 
-            set_down(0, i, left2[i]);
+> Оопэшный классифицированный плюсовичок:
+set_down(0, i, left2[i]);
             set_down(i, 2, front2[i]);
             set_down(2, i, right2[i]);
             set_down(i, 0, back2[i]);
         }
     }
     void down_rotation_left(){
-        int front1[3], front2[3], right1[3], right2[3], back1[3], back2[3], left1[3], left2[3];
-
-        for (int i = 0; i < 3; ++i){
-            front1[i] = get_front(2, i);
-            right1[i] = get_right(2, i);
-            back1[i] = get_back(2, i);
-            left1[i] = get_left(2, i);
-
-            front2[i] = get_down(0, i);
-            right2[i] = get_down(i, 2);
-            back2[i] = get_down(2, i);
-            left2[i] = get_down(i, 0);
-        }
-
-        for (int i = 0; i < 3; ++i){
-            set_right(2, i, back1[i]);
-            set_back(2, i, left1[i]);
-            set_left(2, i, front1[i]);
-            set_front(2, i, right1[i]);
-
-            set_down(0, i, right2[i]);
-            set_down(i, 2, back2[i]);
-            set_down(2, i, left2[i]);
-            set_down(i, 0, front2[i]);
-        }
+        this->down_rotation_right();
+        this->down_rotation_right();
+        this->down_rotation_right();
     }
 
     void front_rotation_right(){
@@ -304,31 +300,9 @@ public:
         }
     }
     void front_rotation_left(){
-        int up1[3], up2[3], right1[3], right2[3], down1[3], down2[3], left1[3], left2[3];
-
-        for (int i = 0; i < 3; ++i){
-            up1[i] = get_up(2, i);
-            right1[i] = get_right(i, 0);
-            down1[i] = get_down(0, i);
-            left1[i] = get_left(i, 2);
-
-            up2[i] = get_front(0, i);
-            right2[i] = get_front(i, 2);
-            down2[i] = get_front(2, i);
-            left2[i] = get_front(i, 0);
-        }
-
-        for (int i = 0; i < 3; ++i){
-            set_up(2, i, right1[i]);
-            set_right(i, 0, down1[i]);
-            set_down(0, i, left1[i]);
-            set_left(i, 2, up1[i]);
-
-            set_front(0, i, right2[i]);
-            set_front(i, 2, down2[i]);
-            set_front(2, i, left2[i]);
-            set_front(i, 0, up2[i]);
-        }
+        this->front_rotation_right();
+        this->front_rotation_right();
+        this->front_rotation_right();
     }
 
     void back_rotation_right(){
@@ -359,31 +333,9 @@ public:
         }
     }
     void back_rotation_left(){
-        int up1[3], up2[3], right1[3], right2[3], down1[3], down2[3], left1[3], left2[3];
-
-        for (int i = 0; i < 3; ++i){
-            up1[i] = get_up(0, i);
-            right1[i] = get_right(i, 2);
-            down1[i] = get_down(2, i);
-            left1[i] = get_left(i, 0);
-
-            up2[i] = get_back(0, i);
-            right2[i] = get_back(i, 0);
-            down2[i] = get_back(2, i);
-            left2[i] = get_back(i, 2);
-        }
-
-        for (int i = 0; i < 3; ++i){
-            set_up(0, i, right1[i]);
-            set_right(i, 2, down1[i]);
-            set_down(2, i, left1[i]);
-            set_left(i, 0, up1[i]);
-
-            set_back(0, i, right2[i]);
-            set_back(i, 0, down2[i]);
-            set_back(2, i, left2[i]);
-            set_back(i, 2, up2[i]);
-        }
+        this->back_rotation_right();
+        this->back_rotation_right();
+        this->back_rotation_right();
     }
 
     void pif_paf_right(){
@@ -441,40 +393,182 @@ public:
             return true;
         return false;
     }
-    /**
-    void down_cross(){
-        if (down_cross_completed())
-            break;
 
-        int color = get_down(1, 1);
-        std::vector<int> color_front_ij;
-        std::vector<int> color_back_ij;
-        std::vector<int> color_right_ij;
-        std::vector<int> color_left_ij;
-        std::vector<int> color_up_ij;
-        std::vector<int> color_down_ij;
-
-        for (int i = 1; i < 9; i += 2){
-            if (get_front(i / 3, i % 3) == color)
-                color_front_ij.push_back(i / 3, i % 3);
-            if (get_back(i / 3, i % 3) == color)
-                color_back_ij.push_back(i / 3, i % 3);
-            if (get_right(i / 3, i % 3) == color)
-                color_right_ij.push_back(i / 3, i % 3);
-            if (get_left(i / 3, i % 3) == color)
-                color_left_ij.push_back(i / 3, i % 3);
-            if (get_up(i / 3, i % 3) == color)
-                color_up_ij.push_back(i / 3, i % 3);
-            if (get_down(i / 3, i % 3) == color)
-                color_down_ij.push_back(i / 3, i % 3);
+> Оопэшный классифицированный плюсовичок:
+void random_generation()
+    {
+        int run = 5;
+        int number_rotate = 0;
+        for (int i = 0; i < run; i++)
+        {
+            number_rotate = (rand() * 100) % 12;
+            switch (number_rotate)
+            {
+                case 0:
+                    this->up_rotation_left();
+                    break;
+                case 1:
+                    this->left_rotation_up();
+                    break;
+                case 2:
+                    this->down_rotation_right();
+                    break;
+                case 3:
+                    this->down_rotation_left();
+                    break;
+                case 4:
+                    this->back_rotation_right();
+                    break;
+                case 5:
+                    this->up_rotation_right();
+                    break;
+                case 6:
+                    this->left_rotation_down();
+                    break;
+                case 7:
+                    this->right_rotation_up();
+                    break;
+                case 8:
+                    this->right_rotation_down();
+                    break;
+                case 9:
+                    this->back_rotation_left();
+                    break;
+                case 10:
+                    this->front_rotation_left();
+                    break;
+                case 11:
+                    this->front_rotation_right();
+            }
         }
-        if (color_down_ij.size() != 0){
-            int i = color_down_ij.front(), j = color_down_ij.front();
-        }
-    }*/
+    }
 };
 
-std::ostream& operator<< (std::ostream& stream, cube cub){
+bool is_equal(std::vector<Cube> first, std::vector<Cube> second)
+{
+    for (int i = 0; i < first.size(); i++)
+    {
+        for (int j = 0; j < second.size(); j++)
+        {
+            if (first[i] == second[j])
+            {
+               return true;
+            }
+        }
+    }
+    return false;
+}
+
+std::string getting_ways(Cube cub)
+{
+    std::string answer;
+    std::queue<Cube> the_first_queue;
+    std::queue<Cube> the_second_queue;
+
+    Cube temp;
+
+    if (temp == cub)
+        return answer;
+
+    std::vector<Cube> the_first_set;
+    std::vector<unsigned short int> the_first_set_way;
+    std::vector<Cube> the_second_set;
+    std::vector<unsigned short int> the_second_set_way;
+
+    the_first_queue.push(cub);
+    the_second_queue.push(temp);
+    the_first_set.push_back(cub);
+    the_second_set.push_back(temp);
+
+    bool flag = true;
+
+    Cube rotate_cube;
+    while (flag)
+    {
+        rotate_cube = the_first_queue.front();
+        the_first_queue.pop();
+
+        rotate_cube.right_rotation_up();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.right_rotation_down();
+        rotate_cube.right_rotation_down();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.right_rotation_up();
+
+        rotate_cube.left_rotation_up();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.left_rotation_down();
+        rotate_cube.left_rotation_down();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.left_rotation_up();
+
+        rotate_cube.up_rotation_right();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.up_rotation_left();
+        rotate_cube.up_rotation_left();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.up_rotation_right();
+
+        rotate_cube.down_rotation_right();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.down_rotation_left();
+        rotate_cube.down_rotation_left();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.down_rotation_right();
+
+> Оопэшный классифицированный плюсовичок:
+rotate_cube.front_rotation_right();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.front_rotation_left();
+        rotate_cube.front_rotation_left();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.front_rotation_right();
+
+        rotate_cube.back_rotation_left();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.back_rotation_right();
+        rotate_cube.back_rotation_right();
+        the_first_queue.push(rotate_cube);
+        the_first_set.push_back(rotate_cube);
+        rotate_cube.back_rotation_left();
+
+        if (is_equal(the_first_set, the_second_set))
+            flag = false;
+    }
+
+    return answer;
+}
+
+void assembling(Cube cub)
+{
+        std::string way;
+        way = getting_ways(cub);
+
+        if (way.empty())
+        {
+            std::cout << "cube is normal";
+            return;
+        }
+
+        for (int i = 0; i < way.size(); i++)
+        {
+
+        }
+        std::cout << way;
+}
+
+std::ostream& operator<< (std::ostream& stream, Cube cub){
     for (int i = 0; i < 3; ++i){
         stream << "\t    ";
         for (int j = 0; j < 3; ++j)
@@ -515,29 +609,23 @@ int main(){
     int up[3][3] = {{2, 2, 2}, {2, 2, 2}, {2, 2, 2}};
     int down[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
 
-    cube cub(front, back, right, left, up, down);
-    std::cout << cub << "\n\n";
-    std::cout << cub.down_cross_completed() << "\n\n";
+    Cube cub(front, back, right, left, up, down);
 
-    /* cub.right_rotation_up();
-    std::cout << cub << "\n\n";
+    //cub.random_generation();
 
     cub.right_rotation_down();
+
     std::cout << cub << "\n\n";
-    cub.left_rotation_down();
-    std::cout << cub << "\n\n";
-    cub.left_rotation_up();
-    std::cout << cub << "\n\n";
-    cub.up_rotation_right();
-    std::cout << cub << "\n\n";
-    cub.up_rotation_left();
-    std::cout << cub << "\n\n";
-    cub.down_rotation_right();
-    std::cout << cub << "\n\n";
-    cub.down_rotation_left();
-    std::cout << cub << "\n\n";
-    cub.back_rotation_right();
-    std::cout << cub << "\n\n";
-    cub.back_rotation_left();
-    std::cout << cub << "\n\n"; */
+
+    //cub.right_rotation_up();
+    //cub.right_rotation_up();
+    //cub.right_rotation_up();
+    //cub.left_rotation_up();
+    //cub.back_rotation_left();
+
+    assembling(cub);
+
+    //std::cout << cub.down_cross_completed() << "\n\n";
+
+
 }
